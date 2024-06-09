77541eec23287dece9dc5b7401c60e4f81bb60e5
wangjianbo <jbwang962@gmail.com>
2024-06-10 00:48:51 +0800

    add:my expection with parse erro

---
+++ common/myExpection/expection.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <exception>
+using namespace std;
+// 定义宏，用于声明和注册测试用例
+#define myExpection(name) \
+    class name : public std::exception  { \
+    private:\
+        std::string message;\
+    public:\
+        name(const std::string& msg) : message(msg) {}\
+        const char* what() const noexcept override {\
+            return message.c_str();\
+        }\
+    };
+    

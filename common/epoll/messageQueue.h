fc40ecf6fd7c8ec27625fa539868e7f98c872bde
wangjianbo <jbwang962@gmail.com>
2024-06-10 00:46:53 +0800

    add:epoll server and client with tcp

---
+++ common/epoll/messageQueue.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+class MessageQueue {
+public:
+    // ThreadPool* msgpool;
+    void push(int fd) {
+        std::unique_lock<std::mutex> lock(mutex_);
+        queue_.push(fd);
+        lock.unlock();
+        condition_.notify_one();
+        // msgpool->notify();
+    }
+
+    int pop() {
+        std::unique_lock<std::mutex> lock(mutex_);
+        while (queue_.empty()) {
+            condition_.wait(lock);
+        }
+        int fd = queue_.front();
+        queue_.pop();
+        return fd;
+    }
+    bool empty() {
+        std::unique_lock<std::mutex> lock(mutex_);
+        return queue_.empty();
+    }
+    static MessageQueue& Instance() {
+        static MessageQueue instance;
+        return instance;
+    }
+
+private:
+    std::queue<int> queue_;
+    std::mutex mutex_;
+    std::condition_variable condition_;
+};
---
+++ common/epoll/test.cpp
@@ -0,0 +1,9 @@
+#include "epollServer.h"
+#include "responseThreadPool.h"
+#include "M_connect.h"
+
+int main() {
+    epollServer eps=epollServer(8080);
+    [&eps]() { eps.runall(); }();
+    return 0;
+}

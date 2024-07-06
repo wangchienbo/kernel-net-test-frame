779b4b280c27706339f7db4a63c395b8099efb9b
wangjianbo <jbwang962@gmail.com>
2024-07-06 20:25:21 +0800

    Delete kernel directory

--- kernel/arch/sw_64/include/asm/vmalloc.h
+++
@@ -1,5 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _ASM_SW64_VMALLOC_H
-#define _ASM_SW64_VMALLOC_H
-
-#endif /* _ASM_SW64_VMALLOC_H */
--- kernel/rust/kernel/kasync.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-
-//! Kernel async functionality.
-
-#[cfg(CONFIG_NET)]
-pub mod net;
